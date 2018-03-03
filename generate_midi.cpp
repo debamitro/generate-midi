255786888b2eee1badf31f57679a4b7244a20ba5
Debamitro Chakraborti <debamitro@gmail.com>
2018-03-02 21:46:50 -0500

    Modified into class and method, with simple example

--- generate_midi.cpp
+++ generate_midi.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <vector>
 
 class MidiFile
 {
@@ -6,22 +7,55 @@ public:
     MidiFile (const char * fileName);
     ~MidiFile ();
 
+    void add_note(unsigned char key, unsigned char duration = 0x08);
     void generate();
 private:
     void write_header();
     void write_tracks();
 
+    struct Event {
+        unsigned char delta_;
+        unsigned char status_;
+        unsigned char data1_;
+        unsigned char data2_;
+
+        Event (unsigned char delta,
+               unsigned char status,
+               unsigned char data1,
+               unsigned char data2) :
+            delta_(delta),
+            status_(status),
+            data1_(data1),
+            data2_(data2) { }
+        void write_to (std::ofstream & file) const;
+    };
+
+    struct Track {
+        int length;
+        std::vector<Event> events;
+
+        void write_to (std::ofstream & file) const;
+    };
+
     std::ofstream file;
+    std::vector<Track> tracks;
 };
 
 int main () {
     MidiFile midiFile("out.mid");
+    midiFile.add_note(0x3B);
+    midiFile.add_note(0x3E);
+    midiFile.add_note(0x40);
+    midiFile.add_note(0x3C,0x02);
+    midiFile.add_note(0x3B,0x02);
+    midiFile.add_note(0x3E,0x02);
+    midiFile.add_note(0x3C,0x10);
     midiFile.generate();
 
     return 0;
 }
 
-MidiFile::MidiFile (const char * fileName) : file(fileName) {
+MidiFile::MidiFile (const char * fileName) : file(fileName), tracks(1) {
 }
 
 MidiFile::~MidiFile () {
@@ -31,6 +65,11 @@ MidiFile::~MidiFile () {
 void MidiFile::generate () {
     write_header ();
     write_tracks ();
+}
+
+void MidiFile::add_note(const unsigned char key, const unsigned char duration) {
+    tracks[0].events.emplace_back(Event(0x00,0x90,key,0x40));
+    tracks[0].events.emplace_back(Event(duration,0x80,key,0x40));
 }
 
 void MidiFile::write_header() {
@@ -52,23 +91,28 @@ void MidiFile::write_header() {
 }
 
 void MidiFile::write_tracks() {
+    for (const auto & track : tracks) {
+        track.write_to(file);
+    }
+}
+
+void MidiFile::Track::write_to(std::ofstream & file) const {
     file.write("MTrk",4);
 
     // length
     file.put(0);
     file.put(0);
     file.put(0);
-    file.put(8);
+    file.put(events.size());
 
-    file.put(0x00);
+    for (const auto & event : events) {
+        event.write_to(file);
+    }
+}
 
-    file.put(0x90);
-    file.put(0x3C);
-    file.put(0x40);
-
-    file.put(0x7F);
-
-    file.put(0x80);
-    file.put(0x3C);
-    file.put(0x40);
+void MidiFile::Event::write_to(std::ofstream & file) const {
+    file.put(delta_);
+    file.put(status_);
+    file.put(data1_);
+    file.put(data2_);
 }
